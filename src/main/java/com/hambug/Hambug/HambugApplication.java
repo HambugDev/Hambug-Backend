433e559ba6c5f7af00f98d7d08c12c6818b0cd29
CHISANW <keuye06380618@gmail.com>
2025-08-31 01:08:47 +0900

    JPA Auditing 활성화 추가
    - 엔터티의 생성 및 수정 시간 관리 자동화를 위해 JPA Auditing을 활성화.
    - `@EnableJpaAuditing` 어노테이션을 메인 애플리케이션 클래스에 추가하여 적용.

--- src/main/java/com/hambug/Hambug/HambugApplication.java
+++ src/main/java/com/hambug/Hambug/HambugApplication.java
@@ -2,8 +2,10 @@ package com.hambug.Hambug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HambugApplication {
 
 	public static void main(String[] args) {

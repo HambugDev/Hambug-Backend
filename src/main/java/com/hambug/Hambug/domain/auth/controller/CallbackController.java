02d0cb75a8aad2b9ae12f52ce24c0cfc0ee414d2
CHISANW <keuye06380618@gmail.com>
2025-10-09 20:28:33 +0900

    애플 OAuth2 콜백 엔드포인트 추가
    - 애플 OAuth2 인증 후 콜백 요청을 처리하기 위한 `CallbackController`를 추가.
    - `/login/callback/code/apple` 경로에서 콜백 요청을 수신하고 디버깅 로그를 출력하도록 구현.

---
+++ src/main/java/com/hambug/Hambug/domain/auth/controller/CallbackController.java
@@ -0,0 +1,15 @@
+package com.hambug.Hambug.domain.auth.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController("/login/callback/code")
+public class CallbackController {
+
+    @GetMapping("/apple")
+    public void appleCallback() {
+        log.info("apple callback");
+    }
+}

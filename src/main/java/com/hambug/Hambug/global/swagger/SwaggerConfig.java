7401053efc29bea5c910d5f2bf5991b0e18d4dc8
CHISANW <keuye06380618@gmail.com>
2025-10-13 01:44:06 +0900

    SwaggerConfig에 보안 설정 추가
    - Swagger 문서에 API 인증을 위한 Security Scheme과 Security Requirement를 추가.
    - Access Token과 Refresh Token을 통한 헤더 기반 인증 방식을 문서화하여 API 소비자에게 명확한 인증 절차 제공.

--- src/main/java/com/hambug/Hambug/global/swagger/SwaggerConfig.java
+++ src/main/java/com/hambug/Hambug/global/swagger/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package com.hambug.Hambug.global.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,14 +20,33 @@ public class SwaggerConfig {
                 .url("https://hambug.p-e.kr")
                 .description("Hambug Production API (HTTPS)");
 
+        SecurityScheme accessTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)       // Authorization header로 받기
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization")
+                .description("Access Token을 입력하세요. (예: `Bearer eyJhbGciOi...`)");
+
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("RefreshToken")
+                .description("Refresh Token을 입력하세요.");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Authorization")
+                .addList("RefreshToken");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Hambug API 명세서")
                         .description("Hambug 서비스의 REST API 문서입니다.")
                         .version("v1")
                 )
-                .servers(java.util.List.of(httpsServer));
+                .servers(List.of(httpsServer))
+                .components(new Components()
+                        .addSecuritySchemes("Authorization", accessTokenScheme)
+                        .addSecuritySchemes("RefreshToken", refreshTokenScheme)
+                )
+                .addSecurityItem(securityRequirement);
     }
-
-
 }
